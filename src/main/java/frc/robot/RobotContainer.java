c0cbe1f3f8ccb650baa0a0b9c3754ca74e56effb
Hank Magan <bryant.magan@gmail.com>
2022-08-11 20:23:25 -0400

    Template swerve code set up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,44 +5,99 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.drivetrain.FieldOrientedDrive;
+import frc.robot.subsystems.DrivetrainSubsystem;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+	private final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+	public final Joystick driverStationJoystick;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+	public RobotContainer() {
+		driverStationJoystick = new Joystick(OIConstants.DRIVER_STATION_JOY);
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+		// The controls are for field-oriented driving:
+		// Left stick Y axis -> forward and backwards movement
+		// Left stick X axis -> left and right movement
+		// Right stick X axis -> rotation
+		drivetrainSubsystem.setDefaultCommand(
+				new FieldOrientedDrive(drivetrainSubsystem,
+						() -> -modifyAxis(getLeftY()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+						() -> -modifyAxis(getLeftX()) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+						() -> -modifyAxis(getRightX()) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+		configureButtonBindings();
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by
+	 * instantiating a {@link GenericHID} or one of its subclasses ({@link
+	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+	 * it to a {@link
+	 * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+	}
+
+	private double getLeftY() {
+		return -driverStationJoystick.getRawAxis(0);
+	}
+
+	private double getLeftX() {
+		return driverStationJoystick.getRawAxis(1);
+	}
+
+	private double getRightY() {
+		return -driverStationJoystick.getRawAxis(2);
+	}
+
+	private double getRightX() {
+		return driverStationJoystick.getRawAxis(3);
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		return new InstantCommand();
+	}
+
+	private static double deadband(double value, double deadband) {
+		if (Math.abs(value) > deadband) {
+			if (value > 0.0) {
+				return (value - deadband) / (1.0 - deadband);
+			} else {
+				return (value + deadband) / (1.0 - deadband);
+			}
+		} else {
+			return 0.0;
+		}
+	}
+
+	private static double modifyAxis(double value) {
+		// Deadband
+		value = deadband(value, 0.05);
+
+		// Square the axis
+		value = Math.copySign(value * value, value);
+
+		return value;
+	}
 }
