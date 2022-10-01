9a13ea16bf41faf4dfb5a102ccc1b79904cb3fb8
vwu1888 <vwu1888@gmail.com>
2022-09-30 21:36:32 -0400

    Fixed pose bug

--- src/main/java/frc/robot/utils/MoveAndShootController.java
+++ src/main/java/frc/robot/utils/MoveAndShootController.java
@@ -12,37 +12,31 @@ import frc.robot.subsystems.VisionSubsystem.VisionSubsystem;
 public class MoveAndShootController {
     private final DrivetrainSubsystem drivetrainSubsystem;
 
-    private Pose2d targetPose;
-    private Pose2d originPose;
-
     private ChassisSpeeds chassisSpeeds;
-    private Pose2d targetOffset;
-    private final Rotation2d zeroRotation = new Rotation2d(0.0);
-
+    private Pose2d robotPose;
     private double vectorMagnitude;
 
     public MoveAndShootController(DrivetrainSubsystem drivetrainSubsystem) {
         this.drivetrainSubsystem = drivetrainSubsystem;
-
-        targetPose = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
-        originPose = new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0));
     }
 
     private void updateTargetLocation() {
         chassisSpeeds = drivetrainSubsystem.getChassisSpeeds();
-        targetPose = originPose.relativeTo(drivetrainSubsystem.getPose());
+        robotPose = drivetrainSubsystem.getPose();
 
-        if (targetPose.getX() > 0) {
+        if (robotPose.getX() > 0) {
             chassisSpeeds.vxMetersPerSecond = -chassisSpeeds.vxMetersPerSecond;
             chassisSpeeds.vyMetersPerSecond = -chassisSpeeds.vyMetersPerSecond;
         }
 
-        double vectorMagnitude = Math.sqrt(Math.pow(chassisSpeeds.vyMetersPerSecond, 2)
-                + Math.pow(chassisSpeeds.vyMetersPerSecond, 2));
+        vectorMagnitude = Math.sqrt(
+                Math.pow(chassisSpeeds.vyMetersPerSecond, 2)
+              + Math.pow(chassisSpeeds.vyMetersPerSecond, 2)
+        );
     }
 
     public double calculateShooterSpeed() {
-        if (targetPose.getX() > 0.2) {
+        if (chassisSpeeds.vyMetersPerSecond > 0.2) {
             return 0.0;
         }
         updateTargetLocation();
@@ -51,7 +45,7 @@ public class MoveAndShootController {
     }
 
     public double calculateAngleOffset() {
-        if (targetPose.getY() > 0.2) {
+        if (chassisSpeeds.vyMetersPerSecond > 0.2) {
             return 0.0;
         }
         updateTargetLocation();
