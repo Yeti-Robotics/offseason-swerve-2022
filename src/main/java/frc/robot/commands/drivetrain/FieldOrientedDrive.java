2c7af9cc6aaca3dd9b9646d393e417495f919cea
vwu0000 <vwu00000@email.cpcc.edu>
2022-10-07 15:39:37 -0400

    Possibly fixed moving while shooting?

--- src/main/java/frc/robot/commands/drivetrain/FieldOrientedDrive.java
+++ src/main/java/frc/robot/commands/drivetrain/FieldOrientedDrive.java
@@ -1,48 +1,41 @@
 package frc.robot.commands.drivetrain;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
 import frc.robot.subsystems.VisionSubsystem.VisionSubsystem;
 import frc.robot.utils.MoveAndShootController;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 public class FieldOrientedDrive extends CommandBase {
+    private static boolean targetLock = false;
     private final DrivetrainSubsystem drivetrainSubsystem;
-
     private final DoubleSupplier translationXSupplier;
     private final DoubleSupplier translationYSupplier;
     private final DoubleSupplier rotationSupplier;
-
     private final ProfiledPIDController targetLockPID =
-            new ProfiledPIDController(
-                    ShooterConstants.TARGETING_P,
-                    ShooterConstants.TARGETING_I,
-                    ShooterConstants.TARGETING_D,
-                    new TrapezoidProfile.Constraints(
-                            2 * Math.PI,
-                            4 * Math.PI
-                    )
-            );
-
+        new ProfiledPIDController(
+            ShooterConstants.TARGETING_P,
+            ShooterConstants.TARGETING_I,
+            ShooterConstants.TARGETING_D,
+            new TrapezoidProfile.Constraints(
+                2 * Math.PI,
+                4 * Math.PI
+            )
+        );
     private final MoveAndShootController moveAndShootController;
-    private static boolean targetLock = false;
 
     public FieldOrientedDrive(
-            DrivetrainSubsystem drivetrainSubsystem,
-            DoubleSupplier translationXSupplier,
-            DoubleSupplier translationYSupplier,
-            DoubleSupplier rotationSupplier) {
+        DrivetrainSubsystem drivetrainSubsystem,
+        DoubleSupplier translationXSupplier,
+        DoubleSupplier translationYSupplier,
+        DoubleSupplier rotationSupplier) {
         this.drivetrainSubsystem = drivetrainSubsystem;
         this.translationXSupplier = translationXSupplier;
         this.translationYSupplier = translationYSupplier;
@@ -53,6 +46,10 @@ public class FieldOrientedDrive extends CommandBase {
         addRequirements(drivetrainSubsystem);
     }
 
+    public static void toggleTargetLock() {
+        FieldOrientedDrive.targetLock = !FieldOrientedDrive.targetLock;
+    }
+
     @Override
     public void execute() {
 //        System.out.println("X Supplier: " + translationXSupplier.getAsDouble());
@@ -60,11 +57,11 @@ public class FieldOrientedDrive extends CommandBase {
 //        System.out.println("Rotation Supplier: " + rotationSupplier.getAsDouble());
 
         double xSpeed = modifyAxis(translationXSupplier.getAsDouble()) *
-                DriveConstants.MAX_VELOCITY_METERS_PER_SECOND;
+            DriveConstants.MAX_VELOCITY_METERS_PER_SECOND;
         double ySpeed = modifyAxis(translationYSupplier.getAsDouble()) *
-                DriveConstants.MAX_VELOCITY_METERS_PER_SECOND;
+            DriveConstants.MAX_VELOCITY_METERS_PER_SECOND;
         double thetaSpeed = modifyAxis(rotationSupplier.getAsDouble()) *
-                DriveConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
+            DriveConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND;
 
 //        System.out.println("Before xSpeed: " + xSpeed);
 //        System.out.println("Before ySpeed: " + ySpeed);
@@ -76,18 +73,14 @@ public class FieldOrientedDrive extends CommandBase {
         // System.out.println("xSpeed: " + xSpeed);
         // System.out.println("ySpeed: " + ySpeed);
         // System.out.println("thetaSpeed: " + thetaSpeed);
-        
-        drivetrainSubsystem.drive(
-                ChassisSpeeds.fromFieldRelativeSpeeds(
-                        xSpeed,
-                        ySpeed,
-                        thetaSpeed,
-                        drivetrainSubsystem.getPose().getRotation())
-        );
-    }
 
-    public static void toggleTargetLock() {
-        FieldOrientedDrive.targetLock = !FieldOrientedDrive.targetLock;
+        drivetrainSubsystem.drive(
+            ChassisSpeeds.fromFieldRelativeSpeeds(
+                xSpeed,
+                ySpeed,
+                thetaSpeed,
+                drivetrainSubsystem.getPose().getRotation())
+        );
     }
 
     public boolean isTargetLock() {
@@ -95,18 +88,22 @@ public class FieldOrientedDrive extends CommandBase {
     }
 
     private double lockToTargetWhileMoving() {
+        double movementOffset = moveAndShootController.calculateDegOffset();
+        double offset = Math.copySign(ShooterConstants.TARGET_OFFSET, movementOffset);
+
         return targetLockPID.calculate(
-                VisionSubsystem.getX(),
-                Math.toDegrees(
-                        Math.atan(ShooterConstants.TARGET_OFFSET / (VisionSubsystem.getDistance() + 24.0))
-                        + moveAndShootController.calculateAngleOffset())
+            VisionSubsystem.getX(),
+            Math.toDegrees(
+                Math.atan(offset / (VisionSubsystem.getDistance() + 24.0))
+                    + movementOffset)
         );
     }
+
     private double lockToTarget() {
         return targetLockPID.calculate(
-                VisionSubsystem.getX(),
-                Math.toDegrees(
-                        Math.atan(ShooterConstants.TARGET_OFFSET / (VisionSubsystem.getDistance() + 24.0)))
+            VisionSubsystem.getX(),
+            Math.toDegrees(
+                Math.atan(ShooterConstants.TARGET_OFFSET / (VisionSubsystem.getDistance() + 24.0)))
         );
     }
 
--- src/main/java/frc/robot/utils/MoveAndShootController.java
+++ src/main/java/frc/robot/utils/MoveAndShootController.java
@@ -1,15 +1,11 @@
 package frc.robot.utils;
 
-import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
-import frc.robot.subsystems.VisionSubsystem.VisionSubsystem;
 
 public class MoveAndShootController {
     private final DrivetrainSubsystem drivetrainSubsystem;
@@ -22,6 +18,7 @@ public class MoveAndShootController {
     private double vectorMagnitude;
     private Rotation2d vectorAngle;
     private Translation2d robotToTargetVector;
+    private Translation2d movementVector;
 
     public MoveAndShootController(DrivetrainSubsystem drivetrainSubsystem) {
         this.drivetrainSubsystem = drivetrainSubsystem;
@@ -32,39 +29,52 @@ public class MoveAndShootController {
         robotPose = drivetrainSubsystem.getPose();
         targetPose = origin.relativeTo(robotPose);
 
-        if (robotPose.getX() > 0) {
-            chassisSpeeds.vxMetersPerSecond = -chassisSpeeds.vxMetersPerSecond;
-            chassisSpeeds.vyMetersPerSecond = -chassisSpeeds.vyMetersPerSecond;
+        robotToTargetVector = new Translation2d(targetPose.getX(), targetPose.getY());
+        movementVector = new Translation2d(-chassisSpeeds.vxMetersPerSecond, -chassisSpeeds.vyMetersPerSecond);
+
+        getAngleBetweenVectors();
+    }
+
+    private void getAngleBetweenVectors() {
+        double angle = Math.min(
+            Math.acos(
+                (movementVector.getX() * robotToTargetVector.getX()) + (movementVector.getY() * robotToTargetVector.getY())
+                / (movementVector.getNorm() * robotToTargetVector.getNorm())) * Math.PI / 2,
+            Math.PI / 9);
+
+        if (angle < 0.08) {
+            vectorAngle = new Rotation2d(0.0);
+            return;
         }
 
-        vectorMagnitude = Math.sqrt(
-                Math.pow(chassisSpeeds.vyMetersPerSecond, 2)
-              + Math.pow(chassisSpeeds.vyMetersPerSecond, 2)
-        );
+        Translation2d referenceAxis = robotToTargetVector.rotateBy(robotPose.getRotation().unaryMinus());
+        Translation2d relativeVector = movementVector.rotateBy(robotPose.getRotation().unaryMinus());
 
-        robotToTargetVector = new Translation2d(targetPose.getX(), targetPose.getY());
+        if (relativeVector.getY() < referenceAxis.getY()) {
+            vectorAngle = new Rotation2d(-angle);
+            return;
+        }
 
-        vectorAngle = new Rotation2d(Math.acos(
-                (chassisSpeeds.vyMetersPerSecond * targetPose.getX())
-                + (chassisSpeeds.vyMetersPerSecond * robotPose.getY())
-                / (vectorMagnitude * robotToTargetVector.getNorm()))
-        );
+        vectorAngle = new Rotation2d(angle);
     }
 
     public double calculateShooterSpeed() {
-        if (chassisSpeeds.vyMetersPerSecond > 0.2) {
-            return 0.0;
-        }
         updateTargetLocation();
 
-        return vectorMagnitude * 1.0;
+        Pose2d predictedTargetPose = targetPose.transformBy(new Transform2d(movementVector, Rotation2d.fromDegrees(0)));
+
+        return predictedTargetPose.getTranslation().getNorm() - targetPose.getTranslation().getNorm();
     }
 
-    public double calculateAngleOffset() {
+    public double calculateDegOffset() {
         updateTargetLocation();
 
+        return vectorAngle.getDegrees();
+    }
 
+    public double calculateRadOffset() {
+        updateTargetLocation();
 
-        return Math.toDegrees(Math.atan(vectorMagnitude / VisionSubsystem.getDistance()));
+        return vectorAngle.getDegrees();
     }
 }
